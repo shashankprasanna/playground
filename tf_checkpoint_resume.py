2639ee1f9e63ce2a935926edefd9523c223cf329
Shashank <shashank4@gmail.com>
2019-03-05 01:06:39 -0800

    Example code to test checkpoint and resume

--- tf_checkpoint_resume.py
+++ tf_checkpoint_resume.py
@@ -1,68 +1,85 @@
 #%% Import packages
-import numpy as np
+
 import tensorflow as tf
-from tensorflow.keras.callbacks import ModelCheckpoint
+import numpy as np
+import os
+import keras
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Input, Dense, Dropout, Activation, Flatten
+from keras.models import Model
+from download_cifar10 import load_data
+
+#%% Training parameters
+batch_size = 32  # orig paper trained all networks with batch_size=128
+epochs = 200
+
+
+#data_augmentation = False
+num_classes = 10
+
 
 #%% Initialize variables
-checkpoint_path = './'
 
-#%% Load and process datasets
-mnist = tf.keras.datasets.mnist
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+dataset_path = '/Users/shshnkp/Projects/playground/datasets/'
+checkpoint_path = '/Users/shshnkp/Projects/playground/checkpoints/'
 
-# Reshape for CNNs
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-input_shape = (28, 28, 1)
+#%% Load and prepare datasets
+(x_train, y_train), (x_test, y_test) = load_data(dataset_path)
+input_shape = x_train.shape[1:]
 
-# Cast to single precision
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
+# Normalize data.
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# Substract pixel mean
+x_train_mean = np.mean(x_train, axis=0)
+x_train -= x_train_mean
+x_test -= x_train_mean
+
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
 #%% Define model
-def mnist_mlp_model():
-    model = tf.keras.models.Sequential([
-      tf.keras.layers.Flatten(input_shape=(28, 28)),
-      tf.keras.layers.Dense(128, activation=tf.nn.relu),
-      tf.keras.layers.Dropout(0.2),
-      tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-    ])
-    return model
-
-def mnist_cnn_model():
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3),
-                    activation='relu',
-                    input_shape=input_shape))
-    model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(tf.keras.layers.Dropout(0.25))
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-    model.add(tf.keras.layers.Dropout(0.5))
-    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
-    return model
-
-#%% Load model
 
 
-#%%
-checkpoint_callback = ModelCheckpoint(filepath='weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss')
+def cifar10_resnet50_model():
+    input_tensor = Input(shape=input_shape)
+    base_model = keras.applications.resnet50.ResNet50(include_top=False,
+                                                      weights='imagenet',
+                                                      input_tensor=input_tensor,
+                                                      input_shape=input_shape,
+                                                      classes=None)
+
+    x = base_model.output
+    x = Flatten()(x)
+    predictions = Dense(10, activation='softmax')(x)
+    mdl = Model(inputs=base_model.input, outputs=predictions)
+    mdl.summary()
+    return mdl
+
+#%% Define Callbacks
+
+# Model Checkpoint callback
+model_name = 'cifar10_model.{epoch:03d}.h5'
+if not os.path.isdir(checkpoint_path):
+    os.makedirs(checkpoint_path)
+filepath = os.path.join(checkpoint_path, model_name)
+checkpoint_callback = ModelCheckpoint(filepath=filepath,
+                                      monitor='val_loss')
 callbacks = [checkpoint_callback]
 
-#%% Load checkpoint
-if checkpoint_path is not None:
-    # Load model:
-    model = load_model(checkpoint_path)
-    # Finding the epoch index from which we are resuming
-    initial_epoch = get_init_epoch(checkpoint_path)
-    # Calculating the correct value of count
-    count = initial_epoch*batches_per_epoch
 
-else:
-    model = mnist_cnn_model()
-    initial_epoch = 0
+#%% Train model
+
+model = cifar10_resnet50_model()
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=epochs, callbacks=callbacks)
+
+#%%
+model.evaluate(x_test, y_test)
 
 #%% Start/resume training
 model.fit(x, y, callbacks=callbacks, initial_epoch=initial_epoch)
