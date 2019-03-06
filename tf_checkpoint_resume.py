d55557c45f0988b5fc6e7d45c33b0d871f0caab5
Shashank <shashank4@gmail.com>
2019-03-06 02:07:23 -0800

    Example code to test checkpoint and resume

--- tf_checkpoint_resume.py
+++ tf_checkpoint_resume.py
@@ -1,49 +1,38 @@
 #%% Import packages
-
-import tensorflow as tf
 import numpy as np
 import os
 import keras
 from keras.callbacks import ModelCheckpoint
-from keras.layers import Input, Dense, Dropout, Activation, Flatten
-from keras.models import Model
+from keras.layers import Input, Dense, Flatten
+from keras.models import Model, load_model
 from download_cifar10 import load_data
 
-#%% Training parameters
-batch_size = 32 
-epochs = 200
-
-
-#data_augmentation = False
-num_classes = 10
-
-
-#%% Initialize variables
-
-dataset_path = '/dltraining/datasets/'
-checkpoint_path = '/dltraining/checkpoints/'
-
 #%% Load and prepare datasets
-(x_train, y_train), (x_test, y_test) = load_data(dataset_path)
-input_shape = x_train.shape[1:]
 
-# Normalize data.
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
 
-# Substract pixel mean
-x_train_mean = np.mean(x_train, axis=0)
-x_train -= x_train_mean
-x_test -= x_train_mean
+def load_prepare_dataset(dataset_path):
+    (x_train, y_train), (x_test, y_test) = load_data(dataset_path)
 
-# Convert class vectors to binary class matrices.
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+    # Normalize data.
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+
+    # Substract pixel mean
+    x_train_mean = np.mean(x_train, axis=0)
+    x_train -= x_train_mean
+    x_test -= x_train_mean
+
+    # Convert class vectors to binary class matrices.
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    return (x_train, y_train), (x_test, y_test)
 
 #%% Define model
 
 
-def cifar10_resnet50_model():
+def cifar10_resnet50_model(input_shape):
+
     input_tensor = Input(shape=input_shape)
     base_model = keras.applications.resnet50.ResNet50(include_top=False,
                                                       weights='imagenet',
@@ -58,28 +47,68 @@ def cifar10_resnet50_model():
     mdl.summary()
     return mdl
 
-#%% Define Callbacks
-
-# Model Checkpoint callback
-model_name = 'cifar10_model.{epoch:03d}.h5'
-if not os.path.isdir(checkpoint_path):
-    os.makedirs(checkpoint_path)
-filepath = os.path.join(checkpoint_path, model_name)
-checkpoint_callback = ModelCheckpoint(filepath=filepath,
-                                      monitor='val_loss')
-callbacks = [checkpoint_callback]
+#%%
 
 
-#%% Train model
-
-model = cifar10_resnet50_model()
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=epochs, callbacks=callbacks)
+def load_resume_model(checkpoint_path):
+    list_of_checkpoint_files = os.listdir(checkpoint_path)
+    checkpoint_epoch_number = max([int(file.split(".")[1]) for file in list_of_checkpoint_files])
+    checkpoint_epoch_path = os.path.join(checkpoint_path,
+                                         'cifar10_model.{:03d}.h5'.format(checkpoint_epoch_number))
+    resume_model = load_model(checkpoint_epoch_path)
+    return resume_model, checkpoint_epoch_number
 
 #%%
-model.evaluate(x_test, y_test)
 
-#%% Start/resume training
-model.fit(x, y, callbacks=callbacks, initial_epoch=initial_epoch)
+
+def define_callbacks(checkpoint_path):
+    # Model Checkpoint callback
+    model_name = 'cifar10_model.{epoch:03d}.h5'
+    if not os.path.isdir(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    filepath = os.path.join(checkpoint_path, model_name)
+    checkpoint_callback = ModelCheckpoint(filepath=filepath,
+                                          save_weights_only=False,
+                                          monitor='val_loss')
+    callbacks = [checkpoint_callback]
+    return callbacks
+
+
+#%%
+if __name__ == "__main__":
+
+    # Training parameters
+    batch_size = 32
+    epochs = 50
+    # data_augmentation = False
+    num_classes = 10
+    dataset_path = '/dltraining/datasets/'
+    checkpoint_path = '/dltraining/checkpoints/'
+    #dataset_path = '/Users/shshnkp/Projects/playground/datasets'
+    #checkpoint_path = '/Users/shshnkp/Projects/playground/checkpoints'
+
+    # Load dataset
+    (x_train, y_train), (x_test, y_test) = load_prepare_dataset(dataset_path)
+    input_shape = x_train.shape[1:]
+
+    # %% Load model
+    if os.path.isdir(checkpoint_path) and any(os.listdir(checkpoint_path)):
+        model, epoch_number = load_resume_model(checkpoint_path)
+    else:
+        model = cifar10_resnet50_model(input_shape)
+        epoch_number = 0
+
+    # Define Callbacks
+    callbacks = define_callbacks(checkpoint_path)
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=epochs, initial_epoch=epoch_number, callbacks=callbacks)
+
+    #%%
+    model.evaluate(x_test, y_test)
+
+    #%% Start/resume training
+    model.fit(x, y, callbacks=callbacks, initial_epoch=initial_epoch)
+
