047eedeece0065a3d41a6640539e9d28add8b84f
Shashank <shashank4@gmail.com>
2019-03-11 00:22:12 -0700

    Example code to test checkpoint and resume

--- tf_checkpoint_resume.py
+++ tf_checkpoint_resume.py
@@ -49,6 +49,7 @@ def cifar10_model(input_shape):
 #%%
 def load_checkpoint_model(checkpoint_path, checkpoint_names):
     list_of_checkpoint_files = os.listdir(checkpoint_path)
+    print(list_of_checkpoint_files)
     checkpoint_epoch_number = max([int(file.split(".")[1]) for file in list_of_checkpoint_files])
     checkpoint_epoch_path = os.path.join(checkpoint_path,
                                          checkpoint_names.format(epoch=checkpoint_epoch_number))
