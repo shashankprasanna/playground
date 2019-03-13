f9902d58e0d6b9209fa8466ae81f43ef1233015a
Shashank <shashank4@gmail.com>
2019-03-12 21:52:21 -0700

    Example code to test checkpoint and resume

--- tf_checkpoint_resume.py
+++ tf_checkpoint_resume.py
@@ -82,7 +82,7 @@ def main():
 
     # Training parameters
     batch_size = 512
-    epochs = 30
+    epochs = 5
     volume_mount_dir = '/dltraining/'
     dataset_path = os.path.join(volume_mount_dir, 'datasets')
     checkpoint_path = os.path.join(volume_mount_dir, 'checkpoints')
